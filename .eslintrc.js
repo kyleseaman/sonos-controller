8eafc559d27f4454b9d00425dd65f7860a3d5de9
kyleseaman <kyleseaman@gmail.com>
2019-05-20 18:29:22 -0400

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = { 
+    "extends": [
+      "airbnb-base",
+      "plugin:react/recommended"
+    ],
+    "env": {
+      "browser": true,
+      "node": true,
+    },
+    "parser": "babel-eslint",
+    "plugins": ["react"],
+  }
