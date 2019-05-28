4498d7824b2250830510b2e09a561f04911b8f8d
kyleseaman <kyleseaman@gmail.com>
2019-05-28 14:37:58 -0700

    more config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,51 +1,52 @@
-var proxy = require('http-proxy-middleware');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const proxy = require('http-proxy-middleware');
 
 module.exports = {
+  developMiddleware: (app) => {
+    app.use(
+      './netlify/functions',
+      proxy({
+        target: 'http://localhost:9000',
+        pathRewrite: {
+          './netlify/functions': '',
+        },
+      }),
+    );
+  },
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: 'Gatsby Default Starter',
+    description: 'Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.',
+    author: '@gatsbyjs',
   },
   proxy: {
-    prefix: "/auth",
-    url: "http://localhost:3000"
+    prefix: '/auth',
+    url: 'http://localhost:3000',
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-react-helmet',
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `images`,
+        name: 'images',
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
-    },
-    developMiddleware: app => {
-      app.use(
-        './netlify/functions',
-        proxy({
-          target: 'http://localhost:9000',
-          pathRewrite: {
-            './netlify/functions': '',
-          },
-        })
-      )
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ],
-}
+};
