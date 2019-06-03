e57924663a697ed2fee49de5996dc355ddbbe451
kyleseaman <kyleseaman@gmail.com>
2019-06-03 11:28:43 -0400

    fix Buffer error

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -5,12 +5,21 @@ import { Link } from 'gatsby';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
+const parseURLToken = (props) => {
+  const queryParams = queryString.parse(props.location.search);
+  if (queryParams.data) {
+    return Buffer.from(queryParams.data, 'base64').toString('ascii');
+  }
+  return 'no data';
+};
+
 const SecondPage = props => (
   <Layout>
     <SEO title="Page two" />
     <p>{console.log(props)}</p>
-    <p>{Buffer.from(queryString.parse(props.location.search).data, 'base64').toString('ascii')}</p>
-    <p>{JSON.stringify(queryString.parse(props.location.search))}</p>
+    {parseURLToken(props)}
+    <br/>
+    <br/>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 );
