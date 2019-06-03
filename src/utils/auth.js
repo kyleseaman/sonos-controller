cb917c89d5da68bd5b0959d4adb0d030ba4b3158
kyleseaman <kyleseaman@gmail.com>
2019-06-03 11:45:52 -0400

    stub out auth and add buffer back

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -4,11 +4,10 @@ const getUser = () => (window.localStorage.sonosUser
   ? JSON.parse(window.localStorage.sonosUser)
   : {});
 
-const setUser = user => (window.localStorage.sonosUser = JSON.stringify(user));
+// const setUser = user => (window.localStorage.sonosUser = JSON.stringify(user));
 
-export const isLoggedIn = () => {
-  if (!isBrowser) return false;
-  const user = getUser();
-  return !!user.accessToken;
-};
-
+// export const isLoggedIn = () => {
+//   if (!isBrowser) return false;
+//   const user = getUser();
+//   return !!user.accessToken;
+// };
