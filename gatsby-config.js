98dd4d4e22db3ae3290f94c0ec98e97f1255e92f
kyleseaman <kyleseaman@gmail.com>
2019-07-22 08:50:08 -0400

    add gatsby font loader

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,6 +42,14 @@ module.exports = {
       },
     },
     'gatsby-plugin-netlify',
+    {
+      resolve: 'gatsby-plugin-web-font-loader',
+      options: {
+        google: {
+          families: ['Roboto'],
+        },
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
