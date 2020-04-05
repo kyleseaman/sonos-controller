27f431a4d73c0ffc632ed31244f4158bd5c7145c
kyleseaman <kyleseaman@gmail.com>
2020-04-05 14:10:00 -0400

    lint

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -6,22 +6,24 @@ import Layout from '../components/layout';
 import SEO from '../components/seo';
 import { setUser } from '../utils/auth';
 
-const parseURLToken = (props) => {
-  const queryParams = queryString.parse(props.location.search);
-  if (queryParams.data) {
-    return JSON.parse(Buffer.from(queryParams.data, 'base64').toString('ascii'));
-  }
-  return {};
+const parseURLToken = props => {
+    const queryParams = queryString.parse(props.location.search);
+    if (queryParams.data) {
+        return JSON.parse(
+            Buffer.from(queryParams.data, 'base64').toString('ascii'),
+        );
+    }
+    return {};
 };
 
 const SecondPage = props => (
-  <Layout>
-    <SEO title="Page two" />
-    {setUser(parseURLToken(props))}
-    <br/>
-    <br/>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
+    <Layout>
+        <SEO title="Page two" />
+        {setUser(parseURLToken(props))}
+        <br />
+        <br />
+        <Link to="/">Go back to the homepage</Link>
+    </Layout>
 );
 
 export default SecondPage;
