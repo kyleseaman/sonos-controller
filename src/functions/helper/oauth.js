7299bef620e47a532d392e6c0598872c1c955a89
kyleseaman <kyleseaman@gmail.com>
2020-04-05 14:11:39 -0400

    update oauth calls

--- src/functions/helper/oauth.js
+++ src/functions/helper/oauth.js
@@ -4,33 +4,37 @@ const sonosApi = 'https://api.sonos.com';
 const siteUrl = process.env.URL || 'http://localhost:8000';
 
 export const config = {
-  clientId: process.env.SONOS_CLIENT_ID,
-  clientSecret: process.env.SONOS_CLIENT_SECRET,
-  tokenHost: sonosApi,
-  tokenPath: `${sonosApi}/login/v3/oauth/access`,
-  authorizePath: `${sonosApi}/login/v3/oauth`,
-  redirect_uri: `${siteUrl}/.netlify/functions/auth-callback`,
+    clientId: process.env.SONOS_CLIENT_ID,
+    clientSecret: process.env.SONOS_CLIENT_SECRET,
+    tokenHost: sonosApi,
+    tokenPath: `/login/v3/oauth/access`,
+    authorizePath: `/login/v3/oauth`,
+    redirect_uri: `${siteUrl}/.netlify/functions/auth-callback`,
 };
 
-const createAuthInstance = (credentials) => {
-  if (!credentials.client.id) {
-    throw new Error('MISSING REQUIRED ENV VARS. Please set SONOS_CLIENT_ID');
-  }
-  if (!credentials.client.secret) {
-    throw new Error('MISSING REQUIRED ENV VARS. Please set SONOS_CLIENT_SECRET');
-  }
+const createAuthInstance = credentials => {
+    if (!credentials.client.id) {
+        throw new Error(
+            'MISSING REQUIRED ENV VARS. Please set SONOS_CLIENT_ID',
+        );
+    }
+    if (!credentials.client.secret) {
+        throw new Error(
+            'MISSING REQUIRED ENV VARS. Please set SONOS_CLIENT_SECRET',
+        );
+    }
 
-  return simpleOauth.create(credentials);
+    return simpleOauth.create(credentials);
 };
 
 export const authInstance = createAuthInstance({
-  client: {
-    id: config.clientId,
-    secret: config.clientSecret,
-  },
-  auth: {
-    tokenHost: config.tokenHost,
-    tokenPath: config.tokenPath,
-    authorizePath: config.authorizePath,
-  },
+    client: {
+        id: config.clientId,
+        secret: config.clientSecret,
+    },
+    auth: {
+        tokenHost: config.tokenHost,
+        tokenPath: config.tokenPath,
+        authorizePath: config.authorizePath,
+    },
 });
