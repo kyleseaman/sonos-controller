5c3f16c969bcc7c53f6fd606312d454a67122bef
kyleseaman <kyleseaman@gmail.com>
2020-04-05 14:11:26 -0400

    prettier files

--- src/functions/auth-callback.js
+++ src/functions/auth-callback.js
@@ -1,34 +1,40 @@
 const { config, authInstance } = require('./helper/oauth');
 
 exports.handler = (event, context, callback) => {
-  const { code } = event.queryStringParameters;
+    const { code } = event.queryStringParameters;
 
-  authInstance.authorizationCode.getToken({
-    code,
-    redirect_uri: config.redirect_uri,
-    client_id: config.clientId,
-    client_secret: config.clientSecret,
-  })
-    .then((result) => {
-      const token = authInstance.accessToken.create(result);
-      return token;
-    })
-    .then((result) => {
-      const result64 = Buffer.from(JSON.stringify(result)).toString('base64');
+    authInstance.authorizationCode
+        .getToken({
+            code,
+            redirect_uri: config.redirect_uri,
+            client_id: config.clientId,
+            client_secret: config.clientSecret,
+        })
+        .then(tokenObject => {
+            const accessToken = authInstance.accessToken.create(tokenObject);
+            return accessToken;
+        })
+        .then(accessToken => {
+            const result64 = Buffer.from(JSON.stringify(accessToken)).toString(
+                'base64',
+            );
+            console.log(result64);
 
-      return callback(null, {
-        statusCode: 302,
-        headers: {
-          Location: `/page-2?data=${result64}`,
-          'Cache-Control': 'no-cache',
-        },
-        body: '',
-      });
-    })
-    .catch(error => callback(null, {
-      statusCode: error.statusCode || 500,
-      body: JSON.stringify({
-        error: error.message,
-      }),
-    }));
+            return callback(null, {
+                statusCode: 302,
+                headers: {
+                    Location: `/page-2?data=${result64}`,
+                    'Cache-Control': 'no-cache',
+                },
+                body: '',
+            });
+        })
+        .catch(error =>
+            callback(null, {
+                statusCode: error.statusCode || 500,
+                body: JSON.stringify({
+                    error: error.message,
+                }),
+            }),
+        );
 };
--- src/functions/auth.js
+++ src/functions/auth.js
@@ -1,20 +1,20 @@
-const { config, authInstance } = require('./helper/oauth');
+import { config, authInstance } from './helper/oauth';
 
-exports.handler = (event, context, callback) => {
-  const authorizationURI = authInstance.authorizationCode.authorizeURL({
-    redirect_uri: config.redirect_uri,
-    scope: 'playback-control-all',
-    state: 'none',
-  });
+export function handler(event, context, callback) {
+    const authorizationURI = authInstance.authorizationCode.authorizeURL({
+        redirect_uri: config.redirect_uri,
+        scope: 'playback-control-all',
+        state: 'none',
+    });
 
-  const response = {
-    statusCode: 302,
-    headers: {
-      Location: authorizationURI,
-      'Cache-Control': 'no-cache',
-    },
-    body: '',
-  };
+    const response = {
+        statusCode: 302,
+        headers: {
+            Location: authorizationURI,
+            'Cache-Control': 'no-cache',
+        },
+        body: '',
+    };
 
-  callback(null, response);
-};
+    callback(null, response);
+}
